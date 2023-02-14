36a8fef9660c740a4f03c06a91b6a759ebf2a1ca
bitdern <acnewell09@gmail.com>
2023-02-14 12:15:47 -0600

    init the searchbar and usefetch components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
+// page components
+// this is where we'd import a search page, which we'll need to create
+
+// styles
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Search Bar</p>
       </header>
     </div>
   );
