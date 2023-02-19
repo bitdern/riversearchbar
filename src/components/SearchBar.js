ad64606fd6ebdfd55ab6732ca0911a9cc91dbe61
bitdern <acnewell09@gmail.com>
2023-02-19 11:52:09 -0600

    final clean up and component documentation.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,19 +1,27 @@
 import React, { useState } from "react";
+
+// styles
 import "./SearchBar.css";
+
+// assets
 import SearchIcon from "@mui/icons-material/Search";
 import CloseIcon from "@mui/icons-material/Close";
 
 function SearchBar({ placeholder, data }) {
+  // state that allows for JSX logic, where the bar only shows results when users change the input (search queries).
   const [filteredData, setFilteredData] = useState([]);
   const [wordEntered, setWordEntered] = useState("");
 
+  // function to accesses the changing input value and filters the data.
   const handleFilter = (event) => {
     const searchWord = event.target.value;
     setWordEntered(searchWord);
+    // boolean value to determine whether or not the query matches any data stored in the database in a case-agnostic manner.
     const newFilter = data.filter((value) => {
       return value.title.toLowerCase().includes(searchWord.toLowerCase());
     });
 
+    // piece of logic that only opens the search drop-down if a user has entered a query value.
     if (searchWord === "") {
       setFilteredData([]);
     } else {
@@ -21,10 +29,14 @@ function SearchBar({ placeholder, data }) {
     }
   };
 
+  // function to clear the input when users click on the "close" icon using second state entry.
   const clearInput = () => {
     setFilteredData([]);
     setWordEntered("");
   };
+
+  // basic search bar component functionality, icons will change based on whether or not a search has been initiated.
+  // search bar's dropdown list is capped at 15 entries to reduce the weight of the component.
   return (
     <div className="search">
       <div className="seachInputs">
